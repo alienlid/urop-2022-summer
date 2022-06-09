3b5d275beca7729ed9dc9c684065100d8fa9d925
alienlid <106131271+alienlid@users.noreply.github.com>
2022-06-09 00:49:53 -0400

    Add files via upload

--- transfer_learning/datasets.py
+++ transfer_learning/datasets.py
@@ -1,5 +1,6 @@
 import numpy as np
 import os
+from PIL import Image
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -8,13 +9,14 @@ import torchvision.transforms as transforms
 import torchvision.datasets as dset
 
 class CIFAR10C(dset.VisionDataset):
-	def __init__(self, root, corruption, transform):
+	def __init__(self, root, train, corruption, severity, transform):
 		super(CIFAR10C, self).__init__(root = root, transform = transform)
-		self.data = np.load(os.path.join(root, corruption + '.npy'))
-		self.targets = np.load(os.path.join(root, 'labels.npy'))
+		(num_of_imgs, folder) = (50000, 'CIFAR-10-C-TRAIN') if train else (10000, 'CIFAR-10-C-TEST')
+		self.data = np.load(os.path.join(root, folder, corruption + '.npy'))[(severity - 1) * num_of_imgs : severity * num_of_imgs]
+		self.targets = np.load(os.path.join(root, folder, 'labels.npy'))
         
 	def __getitem__(self, index):
-		return self.transform(PIL.Image.fromarray(self.data[index])), self.targets[index]
+		return self.transform(Image.fromarray(self.data[index])), self.targets[index]
     
 	def __len__(self):
 		return len(self.data)
@@ -51,7 +53,7 @@ test_dset_scratch = torchvision.datasets.CIFAR10(root = 'data', train = False, d
 train_dset_finetune = torchvision.datasets.CIFAR10(root = 'data', train = True, download = True, transform = transform_train_finetune)
 test_dset_finetune = torchvision.datasets.CIFAR10(root = 'data', train = False, download = True, transform = transform_test_finetune)
 
-def loaders(pretrained, target, corruption):
+def loaders(pretrained, target, corruption = None, severity = 1):
 	if pretrained == 'random_init':
 		test_dataset = torchvision.datasets.CIFAR10(root = 'data', train = False, download = True, transform = transform_test_scratch)
 		test_loader = torch.utils.data.DataLoader(dataset = test_dataset, batch_size = 128)
@@ -59,7 +61,7 @@ def loaders(pretrained, target, corruption):
 			train_dataset = torchvision.datasets.CIFAR10(root = 'data', train = True, download = True, transform = transform_train_scratch)
 			train_loader = torch.utils.data.DataLoader(dataset = train_dataset, batch_size = 128, shuffle = True)
 		else:
-			train_dataset = CIFAR10C(root = 'data/CIFAR-10-C', corruption = corruption, transform = transform_train_scratch)
+			train_dataset = CIFAR10C('data', True, corruption, severity, transform_train_scratch)
 			train_loader = torch.utils.data.DataLoader(dataset = train_dataset, batch_size = 128, shuffle = True)
 	else:
 		test_dataset = torchvision.datasets.CIFAR10(root = 'data', train = False, download = True, transform = transform_test_finetune)
@@ -68,9 +70,10 @@ def loaders(pretrained, target, corruption):
 			train_dataset = torchvision.datasets.CIFAR10(root = 'data', train = True, download = True, transform = transform_train_finetune)
 			train_loader = torch.utils.data.DataLoader(dataset = train_dataset, batch_size = 128, shuffle = True)
 		else:
-			train_dataset = CIFAR10C(root = 'data/CIFAR-10-C', corruption = corruption, transform = transform_train_finetune)
+			train_dataset = CIFAR10C('data', True, corruption, severity, transform_train_finetune)
 			train_loader = torch.utils.data.DataLoader(dataset = train_dataset, batch_size = 128, shuffle = True)
 	return train_loader, test_loader
 
 if __name__ == "__main__":
+	# ~ train_loader, test_loader = loaders('random_init', 'cifar10-c', 'gaussian_noise', severity = 3);
 	pass
