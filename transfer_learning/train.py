576844e7feeb6a3235af6a2876fdeae58c5c3180
Daniel Li <daniel.dongrui.li@dhcp-10-29-105-92.dyn.MIT.EDU>
2022-06-21 14:29:30 -0400

    modified train.py

--- transfer_learning/train.py
+++ transfer_learning/train.py
@@ -12,7 +12,7 @@ from datasets import CIFAR10C, CIFAR10S, transform_train_scratch, transform_trai
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(device)
 
-epochs = 40
+epochs = 30
 learning_rate = 1e-3
 loss = nn.CrossEntropyLoss()
 
@@ -23,12 +23,12 @@ model = get_model('imagenet')
 model = model.to(device)
 not_fc = [param for name, param in model.named_parameters() if name not in ["fc.weight", "fc.bias"]]
 optimizer = torch.optim.SGD([{'params': not_fc}, {'params': model.fc.parameters(), 'lr': learning_rate * 10}], lr = learning_rate, momentum = 0.9, weight_decay = 1e-4)
-scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr = 1e-2, total_steps = 20000)
-train_dataset = CIFAR10C('data', True, 'gaussian_blur', severity, transform_train_scratch)
+scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr = 1e-2, total_steps = 15000)
+train_dataset = CIFAR10C('data', True, 'gaussian_blur', severity, transform_train_finetune)
 train_loader = torch.utils.data.DataLoader(dataset = train_dataset, batch_size = 128, shuffle = True)
-OOD_test_dataset = torchvision.datasets.CIFAR10(root = 'data', train = False, download = True, transform = transform_test_scratch)
+OOD_test_dataset = torchvision.datasets.CIFAR10(root = 'data', train = False, download = True, transform = transform_test_finetune)
 OOD_test_loader = torch.utils.data.DataLoader(dataset = OOD_test_dataset, batch_size = 128)
-IID_test_dataset = CIFAR10C('data', False, 'gaussian_blur', severity, transform_test_scratch)
+IID_test_dataset = CIFAR10C('data', False, 'gaussian_blur', severity, transform_test_finetune)
 IID_test_loader = torch.utils.data.DataLoader(dataset = IID_test_dataset, batch_size = 128)
 for epoch in range(epochs):
 	for x, y in train_loader:
