a3400419cf578b4b8100e905c2ad7bcf3173dec2
Daniel Li <daniel.dongrui.li@dhcp-10-29-105-92.dyn.MIT.EDU>
2022-06-22 18:41:15 -0400

    modified train.py and datasets.py

--- transfer_learning/train.py
+++ transfer_learning/train.py
@@ -7,7 +7,7 @@ import torchvision
 import torchvision.transforms as T
 import torchvision.datasets as datasets
 from models import get_model
-from datasets import CIFAR10C, CIFAR10S, transform_train_scratch, transform_train_finetune, transform_test_scratch, transform_test_finetune
+from datasets import CIFAR10CS, transform_train_scratch, transform_train_finetune, transform_test_scratch, transform_test_finetune
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(device)
@@ -16,7 +16,9 @@ epochs = 30
 learning_rate = 1e-3
 loss = nn.CrossEntropyLoss()
 
-severity = int(os.getenv("SLURM_ARRAY_TASK_ID"))
+severity = int(os.getenv("SLURM_ARRAY_TASK_ID")) % 5 + 1
+shortcut = 2 ** (int(os.getenv("SLURM_ARRAY_TASK_ID")) % 3)
+
 print(f'Severity: {severity}')
 
 model = get_model('imagenet')
@@ -24,11 +26,11 @@ model = model.to(device)
 not_fc = [param for name, param in model.named_parameters() if name not in ["fc.weight", "fc.bias"]]
 optimizer = torch.optim.SGD([{'params': not_fc}, {'params': model.fc.parameters(), 'lr': learning_rate * 10}], lr = learning_rate, momentum = 0.9, weight_decay = 1e-4)
 scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr = 1e-2, total_steps = 15000)
-train_dataset = CIFAR10C('data', True, 'gaussian_blur', severity, transform_train_finetune)
+train_dataset = CIFAR10CS('data', True, 'gaussian_blur', severity, shortcut, transform_test_finetune)
 train_loader = torch.utils.data.DataLoader(dataset = train_dataset, batch_size = 128, shuffle = True)
 OOD_test_dataset = torchvision.datasets.CIFAR10(root = 'data', train = False, download = True, transform = transform_test_finetune)
 OOD_test_loader = torch.utils.data.DataLoader(dataset = OOD_test_dataset, batch_size = 128)
-IID_test_dataset = CIFAR10C('data', False, 'gaussian_blur', severity, transform_test_finetune)
+IID_test_dataset = CIFAR10CS('data', False, 'gaussian_blur', severity, shortcut, transform_test_finetune)
 IID_test_loader = torch.utils.data.DataLoader(dataset = IID_test_dataset, batch_size = 128)
 for epoch in range(epochs):
 	for x, y in train_loader:
@@ -59,4 +61,4 @@ for epoch in range(epochs):
 		correct += (pred.argmax(1) == y.to(device)).sum()
 	print(f'OOD accuracy: {100 * float(correct) / total}%')
 	
-torch.save(model.state_dict(), f'{severity}.pt')
+torch.save(model.state_dict(), f'gaussian_blur/{shortcut}-{severity}.pt')
